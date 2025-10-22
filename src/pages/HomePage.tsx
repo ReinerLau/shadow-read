6107394ea922b6d984f770df1f4f37d28911688d
ReinerLau <lk850593913@gmail.com>
2025-10-22 13:27:24 +0800

    Update HomePage layout to improve button alignment and functionality, changing search button type to 'text' and adding a comment for video import.

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -7,9 +7,16 @@ function HomePage() {
   return (
     <div className="h-dvh flex flex-col">
       {/* header */}
-      <div className="p-2 flex justify-end">
+      <div className="p-2 flex justify-between">
+        {/* 搜索视频 */}
         <Button
-          type="primary"
+          type="text"
+          shape="circle"
+          icon={<div className="i-mdi-magnify text-xl" />}
+        />
+        {/* 导入视频 */}
+        <Button
+          type="default"
           shape="circle"
           icon={<div className="i-mdi-plus text-xl" />}
         />
